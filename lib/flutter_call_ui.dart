19d49adef13341d2408031b9da17825355ab8511
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-09-29 13:29:41 +0200

    fix(theme): Export theme object

--- lib/flutter_call_ui.dart
+++ lib/flutter_call_ui.dart
@@ -4,3 +4,5 @@ export 'package:flutter_call_ui/src/call_core.dart';
 export 'package:flutter_call_ui/src/enums.dart';
 export 'package:flutter_call_ui/src/l10n.dart';
 export 'package:flutter_call_ui/src/models/user.model.dart';
+export 'package:flutter_call_ui/src/theme/base_theme.dart';
+export 'package:flutter_call_ui/src/theme/theme.dart';
